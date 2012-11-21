cf622fa5dc819aca8ca0d049501f4f8dac4a3f9e
sjkurani <sjkurani@gmail.com>
2012-11-21 18:07:18 +0530

    Update levels/instr2.sh

--- levels/instr2.sh
+++ levels/instr2.sh
@@ -2,7 +2,7 @@ setterm -term linux -back green  -fore  white
 echo "------------------------------------LEVEL2--------------------------------------"
 echo "Create a command(file/shell script) inside /bin directory with your entered name"
 echo "and it have to run from any place like a command remember for this you have to  "
-echo "make that file as a executable file(hint:give all permissions to all users(777))"
+echo "make that file as a executable file(hint:give all permissions to all users(555))"
 echo " "
 echo "Your Entered name is \"$name\"                                                  "						
 echo "--------------------------------------------------------------------------------"
